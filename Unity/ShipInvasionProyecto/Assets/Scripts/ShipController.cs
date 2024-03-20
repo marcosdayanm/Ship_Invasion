a6745103d528dfd159117a8cd87dbfd488db5976
Emily Rosenfeld <malkaheluani@MacBook-Pro-3.local>
2024-03-19 20:37:05 -0600

    Menu Inicio unity y creditos

---
+++ Unity/ShipInvasionProyecto/Assets/Scripts/ShipController.cs
@@ -0,0 +1,26 @@
+//Version 1 
+// 19 mar 2024
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] List<int> sequence; //Para que unity lo pueda ver (SerializeField)
+    [SerializeField] GameObject[] buttons; 
+    [SerializeField] TMP_Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
