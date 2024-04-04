07df07689c422246c3de5b9676bcb6ea0d0241e5
marcosdayanm <marcosdayanmann@gmail.com>
2024-04-04 15:35:21 -0600

    APIconnection POST method and login api endpoint

--- Unity/ShipInvasion3D/Assets/Scripts/ApiConnection/APIConnection.cs
+++ Unity/ShipInvasion3D/Assets/Scripts/ApiConnection/APIConnection.cs
@@ -5,6 +5,27 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
 
+
+// clase custom para serializar diccionarios en JSON
+[System.Serializable]
+public class Serialization<TK, TV>
+{
+    public List<TK> keys = new List<TK>();
+    public List<TV> values = new List<TV>();
+
+    public Serialization(Dictionary<TK, TV> dict)
+    {
+        foreach (KeyValuePair<TK, TV> pair in dict)
+        {
+            keys.Add(pair.Key);
+            values.Add(pair.Value);
+        }
+    }
+}
+
+
+
+
 public class APIConnection : MonoBehaviour
 {
     [SerializeField] string apiURL = "localhost:3000";
@@ -14,7 +35,7 @@ public class APIConnection : MonoBehaviour
     private string data;
     CardDetails card;
     Cards cards;
-
+    PlayerDetails player;
 
     public void Start()
     {
@@ -42,6 +63,23 @@ public class APIConnection : MonoBehaviour
         cardName.text = card.CardName;
     }
 
+    
+    IEnumerator PostPlayerLogInCredentials()
+    {
+        endpoint = "/api/player/";
+        idParameter = "";
+
+        Dictionary<string, string> data = new Dictionary<string, string>
+        {
+            {"username", "player1"},
+            {"password", "password1"}
+        };
+
+        yield return StartCoroutine(SendPostRequest(data));
+    }
+
+
+
 
     IEnumerator SendGetRequest()
     {
@@ -59,4 +97,51 @@ public class APIConnection : MonoBehaviour
             data = www.downloadHandler.text;
         }
     }
+
+
+//     IEnumerator SendPostRequest(new Dictionary<string, string> data)
+//     {
+//         // Convertir el diccionario a JSON con nuestra clase personalizada
+//         string jsonData = JsonUtility.ToJson(new Serialization<string, string>(data));
+
+
+//         UnityWebRequest www = new UnityWebRequest(apiURL + endpoint, "POST");
+//         byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+//         www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+//         www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+//         www.SetRequestHeader("Content-Type", "application/json");
+
+//         // Enviar la solicitud
+//         yield return www.SendWebRequest();
+
+//         if(www.result != UnityWebRequest.Result.Success) Debug.Log($"Request failed: {www.error}");
+//         else
+//         {
+//             string responseData = www.downloadHandler.text;
+//             Debug.Log($"Response: {responseData}");
+//             return responseData;
+//         }
+//     }
+
+    // Método de Gil
+    IEnumerator SendPostRequest(new Dictionary<string, string> data)
+    {
+        string jsonData = JsonUtility.ToJson(new Serialization<string, string>(data));
+        UnityWebRequest www = new UnityWebRequest.Put(apiURL + endpoint, jsonData)
+        {
+            www.method = "POST";
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.SendWebRequest();
+
+            if(www.result != UnityWebRequest.Result.Success) Debug.Log($"Request failed: {www.error}");
+            else
+            {
+                string responseData = www.downloadHandler.text;
+                Debug.Log($"Response: {responseData}");
+                return responseData;
+            }
+        }
+    }
+
 }
+
