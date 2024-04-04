013bbc607e6a72f178c76b43896209af7ed06ea3
Emily Rosenfeld <malkaheluani@MacBook-Pro-5.local>
2024-04-04 14:51:40 -0600

    prefabs_package

--- Unity/ShipInvasion3D/Assets/Scripts/Game/CameraOrbit.cs
+++ Unity/ShipInvasion3D/Assets/Scripts/Game/CameraOrbit.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour
 {
-    [SerializeField] Transform boardCenter; // Asigna el centro del tablero aquí
+    [SerializeField] Transform gridCenter; // Asigna el centro del tablero aquí
     [SerializeField] float rotationSpeed = 120.0f; // Velocidad a la que la cámara gira alrededor del tablero
     private bool isRotating = false; // Controla si la cámara está rotando
     private float totalRotation = 0f; // Mide el total de la rotación realizada
-    private float targetRotation = 180f; // El objetivo de rotación en grados
+    private float targetRotation = 90f; // El objetivo de rotación en grados
 
     void Update()
     {
@@ -27,6 +27,7 @@ public class CameraOrbit : MonoBehaviour
     
     void Rotate()
     {
+        targetRotation = targetRotation == 90f ? 0f : 90f;
         isRotating = true;
         totalRotation = 0f;
     }
@@ -36,18 +37,19 @@ public class CameraOrbit : MonoBehaviour
         // Incrementar el ángulo basado en la velocidad y el tiempo
         float angle = rotationSpeed * Time.deltaTime;
         // Asegurarse de no sobrepasar el giro deseado
-        if (totalRotation + angle > targetRotation)
+        if (totalRotation + angle > Mathf.Abs(targetRotation))
         {
             angle = targetRotation - totalRotation;
             isRotating = false; // Detiene la rotación una vez alcanzado el ángulo objetivo
         }
 
             // Calcular la posición orbital de la cámara
-        Vector3 offset = Quaternion.Euler(0, angle, 0) * (transform.position - boardCenter.position);
-        transform.position = boardCenter.position + offset;
+        Vector3 offset = Quaternion.Euler(angle, 0, 0) * (transform.position);
+
+        transform.position = offset;
+      //  transform.position = boardCenter.position + offset;
         // Hacer que la cámara siempre mire hacia el centro del tablero
-        transform.LookAt(boardCenter.position);
-        transform.Rotate(26, 0, 0); // Rotar la cámara 180 grados para que mire hacia el centro
+       transform.LookAt(gridCenter.position);
         totalRotation += angle; // Actualiza el total de la rotación realizada
     }
 }
