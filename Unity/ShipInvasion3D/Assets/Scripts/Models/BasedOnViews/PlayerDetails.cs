1cdb463005a77ce62b7b78ba92605e5d501d9945
marcosdayanm <marcosdayanmann@gmail.com>
2024-04-08 00:06:07 -0600

    Login POST method

--- Unity/ShipInvasion3D/Assets/Scripts/Models/BasedOnViews/PlayerDetails.cs
+++ Unity/ShipInvasion3D/Assets/Scripts/Models/BasedOnViews/PlayerDetails.cs
@@ -1,33 +1,28 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Unity.ShipInvasionProyecto.Assets.Scripts.Models.BasedOnViews
+
+[System.Serializable]
+public class PlayerDetails
 {
+    public int PlayerId;
+    public string PlayerUsername;
+    public DateTime PlayerCreationDate;
+    public int PlayerWins;
+    public int PlayerLosses;
+    public int PlayerCoins;
 
-    [System.Serializable]
-    public class PlayerDetails
-    {
-        public int PlayerId;
-        public string PlayerUsername;
-        public DateTime PlayerCreationDate;
-        public int PlayerWins;
-        public int PlayerLosses;
-        public int PlayerCoins;
-
-        public PurchasedSprite PurchasedSpritePlayerId; 
-        public PurchasedSprite PurchasedSpriteSpriteId;
+    // public PurchasedSprite PurchasedSpritePlayerId; 
+    // public PurchasedSprite PurchasedSpriteSpriteId;
 
 
-        // public List<PurchasedSpriteDetails> PurchasedSprites { get; set; }
-    }
-
-
-    [System.Serializable]
-    public class Players
-    {
-        public List<PlayerDetails> Items;
-    }
-
+    // public List<PurchasedSpriteDetails> PurchasedSprites { get; set; }
 }
+
+
+[System.Serializable]
+public class Players
+{
+    public List<PlayerDetails> Items;
+}
+
