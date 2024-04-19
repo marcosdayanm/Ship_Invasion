bc078d10ace0c5f978ffdd3cdcd00de2d8a09be9
marcosdayanm <marcosdayanmann@gmail.com>
2024-04-19 00:58:39 -0600

    web statistics section

--- database/db_scripts/sample_querries.sql
+++ database/db_scripts/sample_querries.sql
@@ -1,5 +1,7 @@
 -- Sample querries to views
 
+use ship_invasion;
+
 SELECT * FROM view_carddetails WHERE CardQuality = 'Gold';
 SELECT * FROM view_carddetails WHERE CardType = 'Defense';
 SELECT * FROM view_carddetails WHERE (LengthX > 1 AND LengthY > 1);
@@ -7,3 +9,10 @@ SELECT * FROM view_carddetails WHERE (LengthX > 1 AND LengthY > 1);
 SELECT * FROM view_playerdetails WHERE PlayerId = 3;
 SELECT * FROM view_playerdetails WHERE PlayerCreationDate > '2024-02-18';
 
+UPDATE Player SET Wins=15 WHERE Id=1;
+UPDATE Player SET Wins=22 WHERE Id=2;
+UPDATE Player SET Wins=19 WHERE Id=3;
+
+UPDATE Player SET Losses=4 WHERE Id=1;
+UPDATE Player SET Losses=10 WHERE Id=2;
+UPDATE Player SET Losses=9 WHERE Id=3;
