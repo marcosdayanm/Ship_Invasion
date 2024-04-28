90a5adc70e03299bca39a59a9e23bc1af357e01f
marcosdayanm <marcosdayanmann@gmail.com>
2024-04-28 14:14:55 -0600

    Project Settings

--- api/app.js
+++ api/app.js
@@ -22,6 +22,7 @@ async function connectToDB() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   });
 }
 
