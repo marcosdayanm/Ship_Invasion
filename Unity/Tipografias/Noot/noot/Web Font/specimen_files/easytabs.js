62b1ea5a8bd817cd5453856a291b98065a1414cb
Remy Patgher <a01784177@tec.mx>
2024-04-29 11:56:58 -0600

    build del juego

--- Unity/Tipografias/Noot/noot/Web Font/specimen_files/easytabs.js
+++ Unity/Tipografias/Noot/noot/Web Font/specimen_files/easytabs.js
@@ -1,7 +1,3 @@
-(function($){$.fn.easyTabs=function(option){var param=jQuery.extend({fadeSpeed:"fast",defaultContent:1,activeClass:'active'},option);$(this).each(function(){var thisId="#"+this.id;if(param.defaultContent==''){param.defaultContent=1;}
-if(typeof param.defaultContent=="number")
-{var defaultTab=$(thisId+" .tabs li:eq("+(param.defaultContent-1)+") a").attr('href').substr(1);}else{var defaultTab=param.defaultContent;}
-$(thisId+" .tabs li a").each(function(){var tabToHide=$(this).attr('href').substr(1);$("#"+tabToHide).addClass('easytabs-tab-content');});hideAll();changeContent(defaultTab);function hideAll(){$(thisId+" .easytabs-tab-content").hide();}
-function changeContent(tabId){hideAll();$(thisId+" .tabs li").removeClass(param.activeClass);$(thisId+" .tabs li a[href=#"+tabId+"]").closest('li').addClass(param.activeClass);if(param.fadeSpeed!="none")
-{$(thisId+" #"+tabId).fadeIn(param.fadeSpeed);}else{$(thisId+" #"+tabId).show();}}
-$(thisId+" .tabs li").click(function(){var tabId=$(this).find('a').attr('href').substr(1);changeContent(tabId);return false;});});}})(jQuery);
+version https://git-lfs.github.com/spec/v1
+oid sha256:6db6b0c58a834b0808fa8c1cc36757b1b5259020cac7dd6ae7d38bebc6b118f5
+size 1082
--- api/utils/hashPassword.js
+++ api/utils/hashPassword.js
@@ -1,10 +1,3 @@
-import bcrypt from "bcrypt";
-
-const hashPassword = async (password) => {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-}
-
-export {
-    hashPassword
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5195fba69b6306893c74ade86aeba4ee2586f4342eddb45b238f01792d0b6078
+size 192
