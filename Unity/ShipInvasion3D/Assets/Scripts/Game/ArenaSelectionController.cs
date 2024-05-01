b5f15ff9eeadeaa0413533c3e229117365a7e8c0
marcosdayanm <marcosdayanmann@gmail.com>
2024-04-30 19:21:58 -0600

    arenaselection discounting player coins and edit player

---
+++ Unity/ShipInvasion3D/Assets/Scripts/Game/ArenaSelectionController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ArenaSelectionController: MonoBehaviour
+{
+    PlayerDetails user;
+
+    Arena arena;
+
+    ArenasList arenas;
+
+    EditPlayerDetails playerToEdit;
+
+    APIConnection API;
+
+    SceneConnection sceneConnection;
+
+
+
+    void Start()
+    {
+        sceneConnection = GameObject.FindWithTag("SceneConnection").GetComponent<SceneConnection>();
+        // API = GameObject.FindWithTag("APIConnection").GetComponent<APIConnection>();
+        user = JsonUtility.FromJson<PlayerDetails>(PlayerPrefs.GetString("user"));
+    }
+
+
+    public void EnterArena(int arenaId)
+    {
+        StartCoroutine(ValidateEnterArena(arenaId));
+    }
+
+
+
+    public IEnumerator ValidateEnterArena(int arenaId)
+    {
+        arenas = JsonUtility.FromJson<ArenasList>(PlayerPrefs.GetString("arenas"));
+
+        Debug.Log(arenas.items[0]);
+
+        arena = arenas.items[arenaId];
+
+        if (user.PlayerCoins < arena.Cost)
+        {
+            // gestionar lógica de que no se puede jugar
+        }
+        else
+        {
+            StartCoroutine(sceneConnection.API.EditPlayerData(user.PlayerId, user.PlayerCoins - arena.Cost, user.PlayerWins, user.PlayerLosses));
+            sceneConnection.toGame();
+        }
+
+
+        yield return null;
+
+    }
+}
--- Unity/ShipInvasion3D/Assets/Scripts/Models/BasedOnTables/Arena.cs
+++ Unity/ShipInvasion3D/Assets/Scripts/Models/BasedOnTables/Arena.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class Arena
@@ -6,7 +7,14 @@ public class Arena
     public int Id;
     public string Name;
     public int Level;
+
+    public int Cost;
     public int MatchesRequired;
     public int? MusicIdUnity; // Nullable
     public int SpriteId;
+}
+
+public class ArenasList
+{
+    public List<Arena> items;
 }
